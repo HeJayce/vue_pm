38fbb3ea5756b9c15a3cc030696a70e2b41e1769
HeJayce <hejayce@gmail.com>
2021-04-21 17:00:59 +0800

    jayce

---
+++ src/components/result_analysis/resultAnalysis.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Result analysis</h1>
+</template>
+
+<script>
+export default {
+  name: 'resultAnalysis'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import home from '../components/Home.vue'
 import data_analysis_1 from '../components/data_analysis/data_analysis_1.vue'
 import data_analysis_2 from '../components/data_analysis/data_analysis_2.vue'
+import result_analysis from '../components/result_analysis/resultAnalysis.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -12,7 +13,8 @@ const routes = [
     component: home,
     children: [
       { path: '/data_analysis_1', component: data_analysis_1 },
-      { path: '/data_analysis_2', component: data_analysis_2 }
+      { path: '/data_analysis_2', component: data_analysis_2 },
+      { path: '/result_analysis', component: result_analysis }
     ]
   }
 ]

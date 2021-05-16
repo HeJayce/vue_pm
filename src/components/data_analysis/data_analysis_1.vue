994e30bdeb482276eb8213a1ca6bc93dcd1c53e1
HeJayce <hejayce@gmail.com>
2021-05-16 18:09:00 +0800

    jayce

--- src/components/data_analysis/data_analysis_1.vue
+++ src/components/data_analysis/data_analysis_1.vue
@@ -1,40 +1,41 @@
 <template>
   <div>
     <!-- 面包屑导航 -->
-    <el-breadcrumb separator="/">
+    <el-breadcrumb separator='/'>
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item :to="{ path: '/data_analysis_1' }">数据分析一</el-breadcrumb-item>
       <el-breadcrumb-item :to="{ path: '/data_analysis_2' }">数据分析二</el-breadcrumb-item>
     </el-breadcrumb>
     <el-row>
-      <el-button type="primary" @click="getHotpic()">热力图</el-button>
+      <el-button type='primary' @click='getHotpic'>热力图</el-button>
     </el-row>
-      <div v-if = "isShow" class="demo-image__placeholder">
-        <div class="block">
-          <span class="demonstration"></span>
-          <!-- <el-image :src="'data:imgae/jpeg;base64,'+ 'src'"></el-image> -->
-          <el-image :src="src"></el-image>
+    <div v-if='isShow' class='demo-image__placeholder'>
+      <div class='block'>
+        <span class='demonstration'></span>
+        <!-- <el-image :src="'data:imgae/jpeg;base64,'+ 'src'"></el-image> -->
+        <el-image :src='src'></el-image>
       </div>
-      <div class="block">
-        <span class="demonstration"></span>
-        <el-image :src="src">
+      <div class='block'>
+        <span class='demonstration'></span>
+        <el-image :src='src'>
           <template #placeholder>
-            <div class="image-slot">
-              加载中<span class="dot">...</span>
+            <div class='image-slot'>
+              加载中<span class='dot'>...</span>
             </div>
           </template>
         </el-image>
       </div>
-</div>
+    </div>
   </div>
 </template>
 <script>
 import axios from 'axios'
+
 export default {
   name: 'data_analysis_1',
   data () {
     return {
-      baseURL: 'http://8.129.117.120:8090/',
+      baseURL: 'http://127.0.0.1:8000/',
       src: this.src,
       isShow: false
     }
@@ -43,8 +44,8 @@ export default {
     this.getHotpic()
   },
   methods: {
-    getHotpic: function () {
-      var that = this
+    getHotpic () {
+      const that = this
       axios.get(this.baseURL + 'data/get_data').then(function (result) {
         console.log(result.data.data)
         that.src = result.data.data
